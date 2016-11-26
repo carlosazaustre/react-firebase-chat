26d502ccb35ca34fb13f19e9318cdeb28694e23b
Carlos Azaustre <cazaustre@gmail.com>
2016-11-26 13:42:21 +0100

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  entry: ['./src/index.jsx'],
+  output: {
+    filename: 'app.js',
+    path: './public',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      { test: /(\.js|.jsx)$/, exclude: /node_modules/, loaders: ['babel'] }
+    ]
+  },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    inline: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/assets/index.html' })
+  ]
+}
